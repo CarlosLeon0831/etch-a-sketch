83f852ba381fab3db94f7961f7bc6f0d85afcae8
Carlos <carlosleon0831@gmail.com>
2022-08-02 12:54:21 -0500

    Add input

--- script.js
+++ script.js
@@ -1,24 +1,82 @@
-const grid = document.getElementById("grid");
+const DEFAULT_COLOR = "#636363";
+const DEFAULT_MODE = "color";
+const DEFAULT_SIZE = 16;
 
+let color = DEFAULT_COLOR;
+let mode = DEFAULT_MODE;
+let size = DEFAULT_SIZE;
+
+// Grid
+const grid = document.getElementById("grid");
 function makeRows(rows, cols) {
   grid.style.setProperty("--grid-rows", rows);
   grid.style.setProperty("--grid-cols", cols);
   for (c = 0; c < rows * cols; c++) {
     let cell = document.createElement("div");
     grid.appendChild(cell).className = "grid__item";
-    cell.addEventListener('mouseover', changeColor);
-    cell.addEventListener('mousedown', changeColor);
+    cell.addEventListener("mouseover", changeColor);
+    cell.addEventListener("mousedown", changeColor);
+  }
+}
+makeRows(16, 16);
+
+// Change options
+function setColor(newColor) {
+  color = newColor;
+}
+function setMode(newMode) {
+  activateButton(newMode);
+  mode = newMode;
+}
+function setSize(newSize) {
+  size = newSize;
+}
+
+function changeSize(value) {
+  setSize(value);
+  updateSizeValue(value);
+  reloadGrid();
+}
+
+function updateSizeValue(value) {
+  sizeValue.innerHTML = `${value} x ${value}`;
+}
+
+function reloadGrid() {
+  clearGrid();
+  setupGrid(size);
+}
+
+// Draw on mouse down
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+
+function changeColor(e) {
+  if (e.type === "mouseover" && !mouseDown) return;
+  if (mode === "color") {
+    e.target.style.backgroundColor = colorSelection;
+  } else if (mode === "eraser") {
+    e.target.style.backgroundColor = "#e0e0e0";
   }
 }
 
-let mouseDown = false
-document.body.onmousedown = () => (mouseDown = true)
-document.body.onmouseup = () => (mouseDown = false)
+// Buttons
+function activateButton(newMode) {
+  if (mode === "color") {
+    colorBtn.classList.remove("active");
+  } else if (mode === "eraser") {
+    eraserBtn.classList.remove("active");
+  }
 
-function changeColor(e) {
-    if (e.type === 'mouseover' && !mouseDown) return
-    e.target.style.backgroundColor = 'grey';
+  if (newMode === "color") {
+    colorBtn.classList.add("active");
+  } else if (mode === "eraser") {
+    eraserBtn.classList.add("active");
+  }
 }
 
-makeRows(16, 16);
-
+window.onload = () => {
+  grid(DEFAULT_SIZE);
+  activateButton(DEFAULT_MODE);
+};
