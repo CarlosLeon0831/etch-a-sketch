467e4ccdfcbc3c55c90704fc87b2386beb2fb725
Carlos <carlosleon0831@gmail.com>
2022-08-01 22:48:32 -0500

    Add drawing feature

--- script.js
+++ script.js
@@ -6,7 +6,19 @@ function makeRows(rows, cols) {
   for (c = 0; c < rows * cols; c++) {
     let cell = document.createElement("div");
     grid.appendChild(cell).className = "grid__item";
+    cell.addEventListener('mouseover', changeColor);
+    cell.addEventListener('mousedown', changeColor);
   }
 }
 
+let mouseDown = false
+document.body.onmousedown = () => (mouseDown = true)
+document.body.onmouseup = () => (mouseDown = false)
+
+function changeColor(e) {
+    if (e.type === 'mouseover' && !mouseDown) return
+    e.target.style.backgroundColor = 'grey';
+}
+
 makeRows(16, 16);
+
